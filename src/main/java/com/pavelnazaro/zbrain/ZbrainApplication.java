e273ab8ade3888e4f3a7e1fd1ee40572af1841dd
pavelnazaro <pavelnazaro@mail.ru>
2020-10-28 21:42:30 +0300

    Initial commit

---
+++ src/main/java/com/pavelnazaro/zbrain/ZbrainApplication.java
@@ -0,0 +1,13 @@
+package com.pavelnazaro.zbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbrainApplication.class, args);
+    }
+
+}
