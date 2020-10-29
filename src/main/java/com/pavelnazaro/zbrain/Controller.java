2edccf487a55198fb175cf4ef025c84ef9149082
pavelnazaro <pavelnazaro@mail.ru>
2020-10-29 23:36:26 +0300

    Second part done

--- src/main/java/com/pavelnazaro/zbrain/Controller.java
+++ src/main/java/com/pavelnazaro/zbrain/Controller.java
@@ -1,5 +1,6 @@
 package com.pavelnazaro.zbrain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,18 +8,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @org.springframework.stereotype.Controller
 @RequestMapping("/post")
 public class Controller {
+    private UsersService usersService;
 
-    @PostMapping(consumes = "application/json", produces = "application/json")
+    @Autowired
+    public Controller(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @PostMapping()
     @ResponseBody
     public ResponseEntity<String> saveEmail(@RequestBody Email email){
-        if (email.getEmail().equals("test@testemail.com")){
-            return new ResponseEntity<>("{duplicate: true}", HttpStatus.NOT_FOUND);
-        } else if (email.getEmail().equals("testno@testemail.com")) {
-            return new ResponseEntity<>(HttpStatus.OK);
+        List<Users> users = usersService.findByEmail(email.getEmail());
+
+        if (users.size() != 0){
+            return new ResponseEntity<>("{duplicate: true}", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        usersService.save(new Users(email.getEmail()));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
